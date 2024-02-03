8d0a878364a89ca2201eeb3879a789d2beeb4985
Derek Garcia <dlg1206@g.rit.edu>
2024-02-03 17:38:46 -0500

    docs(files): document upload file endpoint

--- backend/src/files/files.controller.ts
+++ backend/src/files/files.controller.ts
@@ -17,12 +17,16 @@ import { AnyFilesInterceptor } from '@nestjs/platform-express';
 export class FilesController {
     constructor(private readonly filesService: FilesService) {}
 
+    /**
+     * Upload Zip File with Multipart file
+     *
+     * @param file Zip binary containing Java Code
+     */
     @Post()
     @UseInterceptors(FileInterceptor('file'))
     create(@UploadedFile() file: Express.Multer.File) {
         return this.filesService.create(file);
     }
-
 
 
     @Post('filecontents')
