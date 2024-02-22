6abe6a2e6edb119a645acb1515d097b2313caa68
Kyler Katz <kkatz8@gatech.edu>
2024-02-22 13:12:46 -1000

    Added more to testing (Currently in python will convert to TS once the logic is complete)

--- testing/toy-dataset-results.py
+++ testing/toy-dataset-results.py
@@ -1,10 +1,11 @@
 import json
 import os
 from collections import defaultdict
-
-CHATGPTPATH = "Files\\Demo\\data.json"
-CODEQLPATH = "Files\\Demo\\results.sarif"
-SRCCODEPATH = "Files\\Demo"
+PROJECT = "CWEToyDataset"
+CHATGPTPATH = f"Files\\{PROJECT}\\data.json"
+CODEQLPATH = f"Files\\{PROJECT}\\results.sarif"
+SRCCODEPATH = f"Files\\{PROJECT}"
+CWESPATH = f"Files\\{PROJECT}\\src\\main\\java\\com\\mycompany\\app"
 
 
 def main():
@@ -12,7 +13,7 @@ def main():
         os.chdir("backend/")
         java_files = get_java_files(SRCCODEPATH)
         chatgpt_results = read_data(CHATGPTPATH)
-        cwes = get_directories_in_dir(SRCCODEPATH)
+        cwes = get_directories_in_dir(CWESPATH)
 
         analyze_chatgpt_results(chatgpt_results, java_files)
 
@@ -47,28 +48,55 @@ def get_directories_in_dir(directory_path):
 def analyze_chatgpt_results(chatgpt_results, java_files):
     true_positives = 0
     false_positives = 0
-    cwe_specific_results = defaultdict(list)
+    true_negatives = 0
+    false_negatives = 0
+
+    cwe_specific_results = defaultdict(lambda: defaultdict(list))
     
-    for file in java_files:
-        file_name = file.split("\\")[-1]
-        
-        # Check that we get a true positive on an expected vulnerabal file
-        if file_name.startswith("BAD") and has_vulnerability(chatgpt_results[file]):
-            print(chatgpt_results[file])
-            true_positives += 1
-        
-        # Check that get a false positive on a non-vulnerable file
-        if file_name.startswith("GOOD") and has_vulnerability(chatgpt_results[file]):
-            print(chatgpt_results[file])
-            false_positives += 1
+    for i, file in enumerate(java_files):
+        split = file.split("\\")
+        file_name = split[-1]
+        cwe = split[-2]
 
-        print(file)
-        if file in chatgpt_results:
-            print(chatgpt_results[file])
+        for res in chatgpt_results:
+            if res['key'] == file_name:
+                val = res['value']
 
-        # print(chatgpt_results[file]
+                # Check that we get a true positive on an expected vulnerabal file
+                if file_name.startswith("BAD") and has_vulnerability(val):
+                    true_positives += 1
+                    cwe_specific_results[cwe]["true_positives"].append(file_name)
+                    print(f"True positive on {file_name} with CWE {cwe}")
+                
+                # Check that get a false positive on a non-vulnerable file
+                elif file_name.startswith("GOOD") and has_vulnerability(val):
+                    false_positives += 1
+                    cwe_specific_results[cwe]["false_positives"].append(file_name)
+                    print(f"False positive on {file_name} with CWE {cwe}")
+                
+                # Check that if we get a false negative on an expected vulnerabal file
+                elif file_name.startswith("BAD") and not has_vulnerability(val):
+                    false_negatives += 1
+                    cwe_specific_results[cwe]["false_negatives"].append(file_name)
+                    print(f"False negative on {file_name} with CWE {cwe}")
 
-def has_vulnerability(file, chatgpt_results):
+                # Check that if we get a true negative on an expected non-vulnerabal file
+                elif file_name.startswith("GOOD") and not has_vulnerability(val):
+                    true_negatives += 1
+                    cwe_specific_results[cwe]["true_negatives"].append(file_name)
+                    print(f"True negative on {file_name} with CWE {cwe}")
+
+
+    print(true_positives, false_positives)
+
+    return {
+    'true_positives': true_positives,
+    'false_positives': false_positives,
+    'cwe_specific_results': cwe_specific_results
+}
+
+
+def has_vulnerability(chatgpt_results):
     return True if len(chatgpt_results) > 0 else False
 
 
--- testing/toy-dataset.ts
+++ testing/toy-dataset.ts
@@ -1,4 +1,99 @@
-const CHATGPTPATH = "../backend/Files/Demo/data.json";
-const CODEQLPATH = "../backend/Files/Demo/results.sarif";
+import * as fs from 'fs';
+import * as path from 'path';
 
-const
+const PROJECT = "CWEToyDataset";
+const CHATGPTPATH = `Files/${PROJECT}/data.json`;
+const CODEQLPATH = `Files/${PROJECT}/results.sarif`;
+const SRCCODEPATH = `Files/${PROJECT}`;
+const CWESPATH = `Files/${PROJECT}/src/main/java/com/mycompany/app`;
+
+async function main() {
+    process.chdir("backend/");
+    const javaFiles = getJavaFiles(SRCCODEPATH);
+    const chatgptResults = readData(CHATGPTPATH);
+    const cwes = getDirectoriesInDir(CWESPATH);
+
+    analyzeChatgptResults(await chatgptResults, await javaFiles);
+
+    console.log(1);
+}
+
+function readData(path: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            if (path.endsWith(".json")) {
+                resolve(JSON.parse(data));
+            } else if (path.endsWith(".sarif")) {
+                // Add logic for parsing SARIF files if needed
+                resolve(data); // Placeholder
+            }
+        });
+    });
+}
+
+function getJavaFiles(path: string): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+        let javaFiles: string[] = [];
+        fs.readdir(path, { withFileTypes: true }, (err, files) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            files.forEach(file => {
+                if (file.isFile() && file.name.endsWith('.java')) {
+                    javaFiles.push(path + '/' + file.name);
+                }
+            });
+            resolve(javaFiles);
+        });
+    });
+}
+
+function getDirectoriesInDir(directoryPath: string): string[] {
+    return fs.readdirSync(directoryPath, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name);
+}
+
+function analyzeChatgptResults(chatgptResults: any[], javaFiles: string[]) {
+    let truePositives = 0;
+    let falsePositives = 0;
+    let trueNegatives = 0;
+    let falseNegatives = 0;
+
+    // TypeScript doesn't have a direct equivalent to Python's defaultdict,
+    // so you might need a custom implementation or workaround
+    let cweSpecificResults: any = {};
+
+    javaFiles.forEach(file => {
+        const split = file.split("\\");
+        const fileName = split[split.length - 1];
+        const cwe = split[split.length - 2];
+
+        chatgptResults.forEach(res => {
+            if (res['key'] === fileName) {
+                const val = res['value'];
+
+                // Implement vulnerability check logic as needed
+                if (fileName.startsWith("BAD") && hasVulnerability(val)) {
+                    truePositives++;
+                    // Initialize or append to cweSpecificResults
+                    console.log(`True positive on ${fileName} with CWE ${cwe}`);
+                }
+                // Other conditions as in your Python code
+            }
+        });
+    });
+
+    console.log(truePositives, falsePositives);
+}
+
+function hasVulnerability(chatgptResults: any[]): boolean {
+    return chatgptResults.length > 0;
+}
+
+main();
