6ae9fd81a3a60f14b8d277fa17fa3ace6895d82b
Kyler Katz <kylerkatz@gmail.com>
2024-03-04 15:50:46 -1000

    Test results are now printed to JSON

--- testing/toy-dataset-results.py
+++ testing/toy-dataset-results.py
@@ -19,6 +19,9 @@ def main():
         codeql_data = read_data(CODEQLPATH)
 
         codeql_results = analyze_codeql_results(codeql_data, java_files)
+        save_results(codeql_results['cwe_specific_results'], "../testing/codeql_results.json")
+        # save_results(codeql_results['cwe_extra_results'], "../testing/codeql_results.json")
+
         # cwes = get_directories_in_dir(CWESPATH)
         print(1)
         
@@ -43,13 +46,29 @@ def get_directories_in_dir(directory_path):
 def has_vulnerability(results):
     return True if len(results) > 0 else False
 
+def dict_to_json(data):
+    # Convert the set to a list
+    for outer_key, inner_dict in data.items():
+        for inner_key, the_set in inner_dict.items():
+            # Convert the set to a list
+            data[outer_key][inner_key] = list(the_set)
+    return data
+
+# Save the results as a JSON file
+def save_results(results, path):
+    data = dict_to_json(results)
+    with open(path, "w") as file:
+        json.dump(data, file, indent=4)
+
 def analyze_codeql_results(codeql_results, java_files):
     true_positives = 0
     false_positives = 0
     true_negatives = 0
     false_negatives = 0
 
-    cwe_specific_results = defaultdict(lambda: defaultdict(list))
+    cwe_specific_results = defaultdict(lambda: defaultdict(set))
+    cwe_extra_results = defaultdict(lambda: defaultdict(set))
+
     results = codeql_results['runs'][0]['results']
     # This tells us all the CWEs that were queried in the codeql analysis
     cwes_queries = [c['properties']['cwe'] for c in codeql_results['runs'][0]['tool']['driver']['rules']]
@@ -73,33 +92,37 @@ def analyze_codeql_results(codeql_results, java_files):
                 # Check that we get a true positive on an expected vulnerabal file
                 if file_name.startswith("BAD") and has_vulnerability(message):
                     true_positives += 1
-                    cwe_specific_results[cwe]["true_positives"].append(file_name)
+                    cwe_specific_results[cwe]["true_positives"].add(file_name)
                     print(f"True positive on {file_name} with CWE {cwe}")
                 
                 # Check that we get a false positive on a non-vulnerable file
                 elif file_name.startswith("GOOD") and has_vulnerability(message):
                     false_positives += 1
-                    cwe_specific_results[cwe]["false_positives"].append(file_name)
+                    cwe_specific_results[cwe]["false_positives"].add(file_name)
                     print(f"False positive on {file_name} with CWE {cwe}")
                 
                 # Check to see if we get a false negative on an expected vulnerabal file
                 elif file_name.startswith("BAD") and not has_vulnerability(message):
                     false_negatives += 1
-                    cwe_specific_results[cwe]["false_negatives"].append(file_name)
+                    cwe_specific_results[cwe]["false_negatives"].add(file_name)
                     print(f"False negative on {file_name} with CWE {cwe}")
 
                 # Check to see if we get a true negative on a non-vulnerabal file
                 elif file_name.startswith("GOOD") and not has_vulnerability(message):
                     true_negatives += 1
-                    cwe_specific_results[cwe]["true_negatives"].append(file_name)
+                    cwe_specific_results[cwe]["true_negatives"].add(file_name)
                     print(f"True negative on {file_name} with CWE {cwe}")
+            
+            else:
+                cwe_extra_results[cwe_query + " query results"][cwe].add(file_name)
 
     print(true_positives, false_positives)
 
     return {
     'true_positives': true_positives,
     'false_positives': false_positives,
-    'cwe_specific_results': cwe_specific_results
+    'cwe_specific_results': cwe_specific_results,
+    'cwe_extra_results': cwe_extra_results 
 }
 
 if __name__ == "__main__":
