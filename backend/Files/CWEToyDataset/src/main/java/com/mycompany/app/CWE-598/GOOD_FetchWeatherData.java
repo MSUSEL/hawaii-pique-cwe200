7299fec83abc6e0e7309c32a616ae5074263128f
Kyler Katz <kylerkatz@gmail.com>
2024-03-13 14:33:56 -1000

    Fixed to CWE-598 Good test cases

---
+++ backend/Files/CWEToyDataset/src/main/java/com/mycompany/app/CWE-598/GOOD_FetchWeatherData.java
@@ -0,0 +1,24 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GOOD_FetchWeatherData {
+    
+    public static void submitSportsCategory(String city) {
+        try {
+            String urlString = "http://weather.com/getWeather?city=" + city;
+                               
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            
+            System.out.println("Sending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + connection.getResponseCode());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        submitSportsCategory("Honolulu");
+    }
+}
