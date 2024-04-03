ee4344d4b6ffdb3fb55c1c55fad59b55627b1feb
Kyler Katz <kylerkatz@gmail.com>
2024-04-03 10:59:17 -1000

    Refactor suspicious string matching logic in data.ts

--- backend/src/code-ql/data.ts
+++ backend/src/code-ql/data.ts
@@ -8,35 +8,6 @@ private string suspicious() {
     ]
 }
 
-// class SensitiveVariable extends Variable {
-//   SensitiveVariable() {
-//     this.getName().matches(suspicious())
-//   }
-// }
-
-// class SensitiveVariableExpr extends Expr {
-//   SensitiveVariableExpr() {
-//     exists(Variable v | this = v.getAnAccess() |
-//     v.getName().matches(suspicious()) and
-//       not this instanceof CompileTimeConstantExpr
-//     )
-//   }
-// }
-
-// class SensitiveStringLiteral extends StringLiteral {
-//   SensitiveStringLiteral() {
-//     // Check for matches against the suspicious patterns
-//     this.getValue().regexpMatch(suspicious()) and
-//     not exists(MethodAccess ma |
-//       ma.getAnArgument() = this and
-//       (
-//         ma.getMethod().hasName("getenv") or
-//         ma.getMethod().hasName("getParameter") or
-//         ma.getMethod().hasName("getProperty") 
-//       )
-//     )
-//   }   
-// }
 
 string suspicious(string fileName) {
 ----------
@@ -60,5 +31,23 @@ class SensitiveVariableExpr extends Expr {
       not this instanceof CompileTimeConstantExpr
     )
   }
+}
+
+class SensitiveStringLiteral extends StringLiteral {
+  SensitiveStringLiteral() {
+    // Check for matches against the suspicious patterns
+    exists(File f | 
+      f = this.getCompilationUnit().getFile() and
+      this.getValue().regexpMatch(suspicious(f.getBaseName()))    
+      ) and
+    not exists(MethodAccess ma |
+      ma.getAnArgument() = this and
+      (
+        ma.getMethod().hasName("getenv") or
+        ma.getMethod().hasName("getParameter") or
+        ma.getMethod().hasName("getProperty") 
+      )
+    )
+  }   
 }
 `
