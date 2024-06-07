7028a8f4df6207c180a7c386f773dfc3c3f05e6f
Kyler Katz <kylerkatz@gmail.com>
2024-06-06 15:21:40 -1000

    Imported props

--- backend/Files/CWEToyDataset/CWEToyDataset/src/main/java/com/mycompany/app/CWE-201/BAD/BAD_EmailHeaderExposure.java
+++ backend/Files/CWEToyDataset/CWEToyDataset/src/main/java/com/mycompany/app/CWE-201/BAD/BAD_EmailHeaderExposure.java
@@ -1,5 +1,6 @@
 import javax.mail.*;
 import javax.mail.internet.*;
+import java.util.Properties;
 
 public class BAD_EmailHeaderExposure {
     public void sendEmailWithSensitiveHeader(String recipient, String sessionToken) {
