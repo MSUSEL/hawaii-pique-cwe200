85042498047bf18cb9d194991702d257773aa315
Kyler Katz <kylerkatz@gmail.com>
2024-07-29 08:28:02 -1000

    Updated variable name.

--- backend/Files/CWEToyDataset/CWEToyDataset/src/main/java/com/mycompany/app/CWE-540/GOOD/GOOD_ClientCredentials.java
+++ backend/Files/CWEToyDataset/CWEToyDataset/src/main/java/com/mycompany/app/CWE-540/GOOD/GOOD_ClientCredentials.java
@@ -1,7 +1,7 @@
 public class GOOD_ClientCredentials {
     public void authenticate() {
         String clientId = System.getenv("CLIENT_ID");
-        String clientSecret = System.getenv("CLIENT_SECRET");
+        String clientPass = System.getenv("CLIENT_SECRET");
         System.out.println("Authenticating with client ID and secret from environment.");
     }
 }
