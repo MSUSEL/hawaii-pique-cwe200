a6f7b0b34f45f41021795c026b37eadbe4d1e4b8
Kyler Katz <kkatz@hawaii.edu>
2025-03-27 02:33:54 +0000

    Fixed syntax error in test

--- backend/Files/CWEToyDataset/CWEToyDataset/src/main/java/com/mycompany/app/CWE-535/BAD/BAD_ShellCommandExposure.java
+++ backend/Files/CWEToyDataset/CWEToyDataset/src/main/java/com/mycompany/app/CWE-535/BAD/BAD_ShellCommandExposure.java
@@ -2,7 +2,7 @@ public class BAD_ShellCommandExposure {
 
     public static void main(String[] args) {
         try {
-            String filePath = "/var/backups/financeapp/data/2024-07-25/backup.tar.gz;"
+            String filePath = "/var/backups/financeapp/data/2024-07-25/backup.tar.gz;";
             String[] command = {"sh", "-c", "cp ", filePath};
             Process process = Runtime.getRuntime().exec(command);
             int exitCode = process.waitFor();
