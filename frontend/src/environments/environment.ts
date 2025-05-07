bb01b2ae0b77e8bccc98d4c6c36feb1bcc307052
KylerKatz <kylerkatz@gmail.com>
2025-05-07 12:18:26 -1000

    Updated frontend configs

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,6 +2,9 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
+// This defines the what the backend API server URL uses.
+// The frontend defaults to http://localhost:4200
 export const environment = {
     production: false,
     // apiUrl: 'http://www.sain.info:5400',
