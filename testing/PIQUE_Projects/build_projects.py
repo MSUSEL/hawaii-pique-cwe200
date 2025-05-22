8317741cb5998301c0fa04e0dc3c41cabb27750d
Kyler Katz <kkatz@hawaii.edu>
2025-05-22 20:52:19 +0000

    Increased coverage to 410 projects

--- testing/PIQUE_Projects/build_projects.py
+++ testing/PIQUE_Projects/build_projects.py
@@ -444,11 +444,11 @@ def main():
     os.makedirs(DATABASE_DIR, exist_ok=True)
     os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-    projects = read_projects(input_projects)[:350]
+    projects = read_projects(input_projects)
     project_index = {p: i for i, p in enumerate(projects)}
 
     # meta_data = download_projects(projects)
-    meta_data = load_meta_data_from_file()[:350]
+    meta_data = load_meta_data_from_file()
     project_results = []
 
     total_projects = len(meta_data)
